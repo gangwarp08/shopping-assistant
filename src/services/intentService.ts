f1dc5b8b88736ec0d0daa2907d3f039a41da4f2b
Prakhar Gangwar <gillyprakhar@gmail.com>
2025-10-28 22:41:33 -0400

    changed from .ts to .js

--- src/services/intentService.ts
+++ src/services/intentService.ts
@@ -1,5 +1,5 @@
 import OpenAI from 'openai';
-import { extractPriceFilter, cleanQueryFromPriceFilter, PriceFilter } from './priceFilterService.ts';
+import { extractPriceFilter, cleanQueryFromPriceFilter, PriceFilter } from './priceFilterService.js';
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY
